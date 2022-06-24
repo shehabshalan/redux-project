ca9066a0a17a75be2ba5fa04edf0a1db65a19443
Shehab Shalan <shehab.shalan@hotmail.com>
2022-06-25 00:49:49 +0200

    post reaction comp

---
+++ src/features/posts/PostReactions.js
@@ -0,0 +1,32 @@
+import { useDispatch } from "react-redux";
+import { addReaction } from "./postsSlice";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  wow: "😮",
+  heart: "❤️",
+  rocket: "🚀",
+  coffee: "☕",
+};
+
+const PostReactions = ({ post }) => {
+  const dispatch = useDispatch();
+
+  const postReactions = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        className="reactionButton"
+        onClick={() =>
+          dispatch(addReaction({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+
+  return <div>{postReactions}</div>;
+};
+export default PostReactions;
